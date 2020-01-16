8d430b748788d74d863121023dcc0d4817cdf580
abraler <571432031@qq.com>
2020-01-16 20:47:16 +0800

    1

---
+++ src/Solution.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        Map<Integer, Integer> rightmostValueAtDepth = new HashMap<Integer, Integer>();
+        int max_depth = -1;
+
+        /* These two stacks are always synchronized, providing an implicit
+         * association values with the same offset on each stack. */
+        Stack<TreeNode> nodeStack = new Stack<TreeNode>();
+        Stack<Integer> depthStack = new Stack<Integer>();
+        nodeStack.push(root);
+        depthStack.push(0);
+
+        while (!nodeStack.isEmpty()) {
+            TreeNode node = nodeStack.pop();
+            int depth = depthStack.pop();
+
+            if (node != null) {
+                max_depth = Math.max(max_depth, depth);
+
+                /* The first node that we encounter at a particular depth contains
+                 * the correct value. */
+                if (!rightmostValueAtDepth.containsKey(depth)) {
+                    rightmostValueAtDepth.put(depth, node.val);
+                }
+
+                nodeStack.push(node.left);
+                nodeStack.push(node.right);
+                depthStack.push(depth+1);
+                depthStack.push(depth+1);
+            }
+        }
+
+        /* Construct the solution based on the values that we end up with at the
+         * end. */
+        List<Integer> rightView = new ArrayList<Integer>();
+        for (int depth = 0; depth <= max_depth; depth++) {
+            rightView.add(rightmostValueAtDepth.get(depth));
+        }
+
+        return rightView;
+    }
+}
